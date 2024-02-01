8154918663df08a6ad446b3e74694b5eda527655
pythonprogramming82 <silvermarsa258@gmail.com>
2024-01-31 22:31:24 -0800

    trip commit

--- Subway.py
+++ Subway.py
@@ -1,43 +1,5 @@
 import sys
 import datetime
-class MetroCard:
-    cost = 3500
-    def __init__(self ,card_type ,credit=0 ,expiration_date=None):
-        self.card_type=card_type
-        self.credit=credit
-        self.expiration_date=expiration_date
-
-    
-    def deduct_credit(self, amount):
-        if self.card_type == "single_table":
-            amount += money
-            if amount == MetroCard.cost:
-                amount -= MetroCard.cost
-                return True
-            else:
-                return False
-            
-    
-    def credit(self, balance):
-        if self.card_type == "credit":
-            balance += money
-            if balance > MetroCard.cost:
-                balance -= MetroCard.cost
-                return True
-            else:
-                return False
-            
-    
-    def time_credit(self,current_date, balance):
-        if self.card_type == "time-credit":
-            if self.expiration_date < current_date:
-                balance += money
-                if balance > MetroCard.cost:
-                    balance -= MetroCard.cost
-                    return True
-            else:
-                return False
-    
 
 class BankAccount:
     def __init__(self,account_number, balance=0):
@@ -47,7 +9,7 @@ class BankAccount:
         self.balance += amount
     
     def withdraw(self,amount):
-        if self.balance >= amount
+        if self.balance >= amount:
             return True
         else:
             False
@@ -77,7 +39,7 @@ class User:
             self.metro_card.deduct_credit(cost)
             return True
         elif self.metro_card.card_type == "credit / time":
-            if self.metro_card.credit < cost or self.metro_card.is_expired(datetime.now():)
+            if self.metro_card.credit < cost or self.metro_card.is_expired(datetime.now()):
                 return False
             self.metro_card.deduct_credit(cost)
             return True
---
+++ main.py
@@ -0,0 +1,5 @@
+from cards import *
+from trip import *
+
+while True:
+    
