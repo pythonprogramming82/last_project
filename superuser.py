2a17ffd614f443d2781dab83a36b3580ed0c3310
nima hassani <hnima1051@gmail.com>
2024-02-03 22:26:52 +0330

    commit

---
+++ superuser.py
@@ -0,0 +1,19 @@
+def Record_trip(self,username,source,destination,fare):
+    if username in self.users:
+        user=self.users[username]
+    if user.metro_card:
+        metro_card=user.metro_card
+        if metro_card.deduct_balance(fare):
+            trip=Trip(source,destination,fare)
+         
+
+            trip.start_trip()
+            trip.end_trip()
+                return "Trip recorded successfully!"
+    else:
+        return "Insufficient balance on the metro card!"
+    else:
+        return "User does not have a metro card!"
+    else:
+        return "User not found"
+print(metro_manager.Record_trip(user1.username,"A","B",5))
