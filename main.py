c8dd6f3a2e809f078e4424c7c71f8982b5ce9dcc
pythonprogramming82 <silvermarsa258@gmail.com>
2024-02-02 19:00:56 -0800

    commit

--- main.py
+++ main.py
@@ -1,16 +1,85 @@
 from cards import *
 from trip import *
+from banksistem import User, Bank
 
 while True:
     option = input("1)login\n2)Bank account management\n3)Metro travel registration\n4)Management\n")
     choice = input("pleas enter the number your chose:")
 
     if choice == "1":
-        print("sssf")
-    elif choice == "2":        
-        print("djf")
+        id = int(input("\tpleas enter the youe ID: "))
+        name = input("\tpleas enter the your name: ")
+        age = int(input("\tpleas enter the your age: "))
+        gender = input("\tpleas enter the your gender: ")
+        obj = User(id, name, age, gender)
+        print(obj.show())
+        print(f"your id is {id}")
+
+
+    elif choice == "2":
+        id_bank = int(input("\tpleas enter the youe ID bank: "))
+        balance = int(input("\tpleas enter your account balance: "))
+        money = int(input("\tpleas enter your money: "))
+        password = int(input("\tpleas enter the your password: "))
+        obj2 = Bank(id_bank, balance, password)
+        print("1.money_in \n 2.money_out")
+        choice = int(input("select any option:"))
+        if choice == 1:
+            if id == id_bank:
+                obj2.money_in(money, password)
+        
+        if choice == 2:
+            if id == id_bank:
+                obj2.money_out(money, password)
+        
+        else:
+            print("your chose is wrong pleas try again...")
+            break
+        
+
     elif choice == "3":
-        print("my name")
+        id_user = int(input("pleas enter your id: "))
+        choice_card = print("pleas chose the card:\n1)single\n2)credit\n3)time_credit")
+        if choice_card == "1":
+            if id_user == id:
+                MetroCard.single(amount=None)
+                print(f"your balance is {balance} Please withdraw 3500 tomans from your account")
+                obj2.money_out(3500, password)
+            
+            else:
+                print("your id is wrong pleas enter youe id again")
+                break
+
+            
+        elif choice_card == "2":
+            if id_user == id:
+                MetroCard.credit(balance=None)
+                print(f"your balance is {balance} Please charge your card")
+                money_card = print("Please select the amount you want to deposit to the card: ")
+                obj2.money_out(money_card, password)
+
+            else:
+                print("your id is wrong pleas enter youe id again")
+                break
+
+
+        elif choice_card == "3":
+            if id_user == id:
+                MetroCard.time_credit(balance=None)
+                print(f"your balance is {balance} Please charge your card")
+                money_card = print("Please select the amount you want to deposit to the card: ")
+                obj2.money_out(money_card, password)
+                print(f"Expiry date of your card is 2044")
+            
+            else:
+                print("your id is wrong pleas enter youe id again")
+                break
+
+
+        else:
+            print("your chose is wrong pleas try again...")
+            break
+
 
     elif choice == "4":
         print("my")
