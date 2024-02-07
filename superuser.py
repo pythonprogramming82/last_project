ccdd5a9305a139411921992ec296afdf05fec41c
pythonprogramming8282 <153461459+pythonprogramming8282@users.noreply.github.com>
2024-02-07 23:18:38 +0330

    superuser.py

--- superuser.py
+++ superuser.py
@@ -1,19 +1,54 @@
-def Record_trip(self,username,source,destination,fare):
-    if username in self.users:
-        user=self.users[username]
-    if user.metro_card:
-        metro_card=user.metro_card
-        if metro_card.deduct_balance(fare):
-            trip=Trip(source,destination,fare)
-         
+from cards import *
+from deletetrip import *
+from changetrip import change_trip
+class Manager():
+    def __init__(self, manager_id, fullname, password):
+        self.manager_id = manager_id
+        self.fullname = fullname
+        self.password = password
 
-            trip.start_trip()
-            trip.end_trip()
-                return "Trip recorded successfully!"
-    elif:
-        return "Insufficient balance on the metro card!"
-    elif:
-        return "User does not have a metro card!"
-    else:
-        return "User not found"
-print(metro_manager.Record_trip(user1.username,"A","B",5))
+
+    def delet_trip(self, manager_id, password):
+        password_manager = 1234
+        manager = 320012
+        if manager_id == manager and password == password_manager:
+            delet_trip()
+            logging.basicConfig(filename="metro.log", level=logging.INFO)
+            logging.info("trip a delet")
+
+
+    def change_trip(self, manager_id, password):
+        password_manager = 1234
+        manager = 320012
+        if manager_id == manager and password == password_manager:
+            change_trip()
+            logging.basicConfig(filename="metro.log", level=logging.INFO)
+            logging.info("the trip change")
+            
+    
+    def add_trip(self, manager_id, password):
+        password_manager = 1234
+        manager = 320012
+        if manager_id == manager and password == password_manager:
+            new_trip = dict(input("pleas enter your new trip: "))
+            cost_matrix.update(new_trip)
+            logging.basicConfig(filename="metro.log", level=logging.INFO)
+            logging.info("a new trip add trips")
+    
+
+    def pickle_txt(self):
+        pickled = pickle.dumps(self)
+        file = open("users.pk", "ab")
+        file.write(pickled)
+        file.close()
+        logging.basicConfig(filename="metro.log", level=logging.INFO)
+        logging.info("trip information is pickle")
+
+
+    def unpickle(file):
+        with open("users.pk", "rb") as file:
+            file = pickle.loads(file)
+            file.read()
+            file.close()
+            logging.basicConfig(filename="metro.log", level=logging.INFO)
+            logging.info("cards is unpickle")
