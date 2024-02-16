2440a65aaf430c545f4f3b15bf77c19e199fd94e
pythonprogramming8282 <153461459+pythonprogramming8282@users.noreply.github.com>
2024-02-16 21:22:43 +0330

    commit

---
+++ logging.py
@@ -0,0 +1,25 @@
+import logging
+logging.basicConfig (level=logging.INFO, format='%(asctime)s - '%(levelname)s - %(message)s')
+
+class Metro:
+    def __init__(self,name)
+        self.name=name
+        logging.info(f"Created a new metro system: {self.name}")
+
+    def open_gate(self,station):
+        logging.info(f"Gate opened at station {station}")
+    
+
+    def close_gate(self,station):
+        logging.info(f"Gate closed at station {station}")
+    
+
+    def run_train(self, train_id, origin, destination):
+        logging.info(f"Train {train_id} is running from {origin} to {destination}")
+
+
+metro_system = Metro("City Metro")
+metro_system.open_gate("Station A")
+metro_system.run_train(1, "Station A", "Station B")
+metro_system.close_gate("Station A")
+    
